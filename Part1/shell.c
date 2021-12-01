d798892c51b9c72d40996d4bbc73917b71c0c658
Arod72564 <aerodriguez@cpp.edu>
2021-12-01 04:38:40 +0000

    Added lsh_split_line function

--- Part1/shell.c
+++ Part1/shell.c
@@ -6,6 +6,8 @@
 #include <string.h>
 
 #define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 
 void lsh_loop(void) {
     char *line;
@@ -59,7 +61,36 @@ char *lsh_read_line(void) {
     }
 }
 
- 
+char **lsh_split_line(char *line) {
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+    
+
+        if (position >= bufsize) {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if(!tokens) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
 
 int main(int argc, char **argv) {
     // Load config files, if any.
