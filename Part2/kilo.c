86223fd79bc3242f92607efdbe12332464261783
Arod72564 <aerodriguez@cpp.edu>
2021-12-07 21:33:49 +0000

    changed to clearing each line as as they are redrawn instead of entire screen

--- Part2/kilo.c
+++ Part2/kilo.c
@@ -132,6 +132,7 @@ void editorDrawRows(struct abuf *ab) {
     for (y = 0; y < E.screenrows; y++) {
         abAppend(ab, "~", 1);
 
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
         }
@@ -142,7 +143,6 @@ void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
