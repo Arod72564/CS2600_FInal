621764978397ebcf46ad8a1245e4259f0c1fde12
Arod72564 <aerodriguez@cpp.edu>
2021-12-07 19:17:41 +0000

    Added global struct to contain editor state, and replaced all occurances of struct object

--- Part2/kilo.c
+++ Part2/kilo.c
@@ -13,7 +13,11 @@
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -23,17 +27,17 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcgetattr");
 
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -59,7 +63,7 @@ char editorReadKey() {
 
 void editorDrawRows() {
     int y;
-    
+
     for (y = 0; y < 24; y++) {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
